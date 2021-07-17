4bd4760efaf724fec74ac27490c42c2b6aa8f9cd
MickShannahan <beasticorn@gmail.com>
2021-07-16 23:38:28 -0600

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'bootstrap'
+import 'jquery'
+import 'popper.js'
+import { createApp } from 'vue'
+// @ts-ignore
+import App from './App.vue'
+import { registerGlobalComponents } from './registerGlobalComponents'
+import { router } from './router'
+
+const root = createApp(App)
+registerGlobalComponents(root)
+
+root
+  .use(router)
+  .mount('#app')
