e5af8aed2a76ac77c23a204c6bd9c280bbdb02eb
MickShannahan <beasticorn@gmail.com>
2021-07-16 23:44:17 -0600

    Clean up changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <header>
     <Navbar />
-    <Test />
   </header>
   <main>
     <router-view />
@@ -27,8 +26,5 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-body{
-  background: url('https://s3-alpha-sig.figma.com/img/954c/c496/f7e7783a32d2f0e961d4f73a8dc25948?Expires=1626048000&Signature=D2ua8dyKQSN9Odqt5ayutWpSTu27lDzdt7xktn3CS6NJTaLvzKQ6EiPp7LiNqox2bp959Wzd3dft43wUJz4VCQEqQ9JtoNEyzKGgpPqfO44OLYIBB8XeN6vMUB62UgXDplcBucyL0RygQbwEMjvDNb5oPHNTFofVpuQKFt2ZecyPps6bW068Dmk5Lu7euzUk24~SMR309VNOibZ3wvttM1GmNbmCvwv0DpU~W6aLNKzsnzO0ViKBjhGre8Zc~ifexwcsNPYz8FtOPSQCqKwQhCrEq85x5wOpXjNuTqky3Bwte~-h9jIyJvbj4MwouZlVU6gLImndk~Mi4vflCy2yTA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA');
-}
 
 </style>
--- src/registerGlobalComponents.js
+++ src/registerGlobalComponents.js
@@ -1,29 +1,12 @@
 export function registerGlobalComponents(root) {
   // https://webpack.js.org/guides/dependency-management/#require-context
   // @ts-ignore
-
-  // const requireComponent = require.context('./components', true, /[a-z0-9]+\.(jsx?|vue)$/i)
-  // requireComponent.keys().forEach(fileName => {
-  //   const componentConfig = requireComponent(fileName)
-  //   const component = componentConfig.default || componentConfig
-  //   // Prefer letting the componentName be that same as the file
-  // const componentName = component.name || fileName
-  //   .substr(fileName.lastIndexOf('/') + 1)
-  //   .replace(/\.\w+$/, '')
-
-  //   // Globally register the component
-  //   root.component(componentName, component)
-  // })
-
   const components = import.meta.globEager('./components/*.vue')
   Object.entries(components).forEach(([fileName, component]) => {
-    // Get name of component, based on filename
-    // "./components/Fruits.vue" will become "Fruits"
     const componentName = component.name || fileName
       .substr(fileName.lastIndexOf('/') + 1)
       .replace(/\.\w+$/, '')
     // Register component on this Vue instance
-    console.log(componentName)
     root.component(componentName, component.default)
   })
 }
