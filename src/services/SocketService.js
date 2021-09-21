335fbd5a7daedc4d5e5752918d96d03c4bd5ad57
Jake Overall <joverall22@gmail.com>
2021-09-20 23:44:41 -0600

    Update SocketService.js

--- src/services/SocketService.js
+++ src/services/SocketService.js
@@ -1,16 +1,15 @@
-import { logger } from '../utils/Logger'
 import Pop from '../utils/Pop'
 import { SocketHandler } from '../utils/SocketHandler'
 
 class SocketService extends SocketHandler {
-  constructor() {
-    super()
-    this
-      .on('error', this.onError)
-  }
+  // NOTE remove comments to enable sockets
+  // constructor() {
+  //   super()
+  //   this
+  //     .on('error', this.onError)
+  // }
 
   onError(e) {
-    logger.error('[SOCKET_ERROR]', e)
     Pop.toast(e.message, 'error')
   }
 }
