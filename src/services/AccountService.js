eda52d83942e0a544dbf2e4aad01583dc8a554d8
Jake Overall <joverall22@gmail.com>
2023-02-23 11:29:45 -0700

    Update AccountService.js

--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -1,4 +1,5 @@
 import { AppState } from '../AppState'
+import { Account } from '../models/Account.js'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
@@ -6,7 +7,7 @@ class AccountService {
   async getAccount() {
     try {
       const res = await api.get('/account')
-      AppState.account = res.data
+      AppState.account = new Account(res.data)
     } catch (err) {
       logger.error('HAVE YOU STARTED YOUR SERVER YET???', err)
     }
