e6fef44cea6c492cf9649b8f256b90d7129664c3
Jake Overall <joverall22@gmail.com>
2023-06-01 17:10:29 -0600

    few more lints

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [vue()],
   build: {
     outDir: 'docs',
     sourcemap: false
