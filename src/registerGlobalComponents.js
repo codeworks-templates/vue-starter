8d4029e3351eb02529a7e054be93e85c171b3247
Jake Overall <joverall22@gmail.com>
2024-01-21 03:38:40 -0700

    Update registerGlobalComponents.js

--- src/registerGlobalComponents.js
+++ src/registerGlobalComponents.js
@@ -2,6 +2,7 @@ export async function registerGlobalComponents(root) {
   return new Promise(async (resolve, reject) => {
     const components = import.meta.glob('./components/**/*.vue')
     for (const fileName in components) {
+      /* @vite-ignore */
       const component = await import(fileName)
       const componentName = (component?.name || fileName)
         .substring(fileName.lastIndexOf('/') + 1)
