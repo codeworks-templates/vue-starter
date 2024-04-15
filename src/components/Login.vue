2947b7f161aac090afeb0551fda372c29cc6597e
MickShannahan <63372695+MickShannahan@users.noreply.github.com>
2024-04-15 07:11:31 -0600

    elvis with login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,13 +17,13 @@ async function logout() {
 <template>
   <span class="navbar-text">
     <button class="btn selectable text-success lighten-30 text-uppercase my-2 my-lg-0" @click="login"
-      v-if="!user.isAuthenticated">
+      v-if="!user?.isAuthenticated">
       Login
     </button>
     <div v-else>
       <div class="dropdown my-2 my-lg-0">
         <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
-          <div v-if="account.picture || user.picture">
+          <div v-if="account?.picture || user?.picture">
             <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
           </div>
         </div>
