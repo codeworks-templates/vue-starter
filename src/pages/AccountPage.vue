92a7cea20d025ddd8a2c64357488ef9b7d9b521e
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-15 11:46:18 -0600

    Added elvis operators to navbar, added v-if on account page

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -8,9 +8,14 @@ const account = computed(() => AppState.account)
 
 <template>
   <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
+    <div v-if="account">
+      <h1>Welcome {{ account.name }}</h1>
+      <img class="rounded" :src="account.picture" alt="" />
+      <p>{{ account.email }}</p>
+    </div>
+    <div v-else>
+      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    </div>
   </div>
 </template>
 
