424b2378b73e0460cc538bba9e6a12e4b16ef404
Jeremy Bob Fowler <86634348+Jeremy-Fowler@users.noreply.github.com>
2024-08-22 15:02:01 -0600

    Removed "banans" from AppState

--- src/AppState.js
+++ src/AppState.js
@@ -5,7 +5,6 @@ export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null,
-  banans: ["apple", "orange", "banana"]
+  account: null
 })
 
